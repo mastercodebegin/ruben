cecada1cf6d200009316e7ad1a7851fe70fdfbd4
selvendiran <selvendiran.r@octavesoftwares.com>
2023-02-16 09:38:21 +0000

    [966579] splash screen development

--- src/packages/blocks/splashscreen/src/assets.tsx
+++ src/packages/blocks/splashscreen/src/assets.tsx
@@ -1,1 +1,3 @@
 export const imgSplash = require("../assets/logo.png");
+export const splashScreenImage = require("../assets/splashScreen.png");
+export const redShadow = require('../assets/redShadow.png')
