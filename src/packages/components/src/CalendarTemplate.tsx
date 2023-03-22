7d040055fb040ff524a556bc4d439a8ad141a905
selvendiran <selvendiran.r@octavesoftwares.com>
2023-03-22 20:25:33 +0530

    [1017260] merchant orders screen

---
+++ src/packages/components/src/CalendarTemplate.tsx
@@ -0,0 +1,208 @@
+import React, { useState, useRef } from "react";
+import {
+  Animated,
+  View,
+  StyleSheet,
+  TouchableOpacity,
+  SafeAreaView,
+  Text,
+  Dimensions,
+  Image,
+  TextInput,
+  TouchableWithoutFeedback,
+  FlatList,
+} from "react-native";
+import { styles as expStyles } from "../../blocks/landingpage/src/ExploreStore/ExplorePage";
+import Calendar from "./Calendar";
+const ScreenWidth = Dimensions.get("window").width;
+const calculatedScreenW = ScreenWidth - 40;
+import {
+  LIGHT_GREY,
+  DARK_RED,
+  WHITE,
+  PRIMARY,
+  SEARCH,
+  EXPLORE_BTN,
+} from "../../blocks/landingpage/src/assets";
+interface CalendarTemplateTypes {
+  children: React.ReactElement<any, any>;
+  header: string;
+  backArrow?: boolean;
+  animateString1: string;
+  animateString2: string;
+  onChangeText: (text: string) => void;
+  additionalHeader?: React.ReactElement<any, any>;
+}
+
+const CalendarTemplate = ({
+  children,
+  header = "",
+  backArrow = false,
+  animateString1 = "",
+  animateString2 = "",
+  onChangeText,
+  additionalHeader
+}: CalendarTemplateTypes) => {
+  const [selected, setSelected] = useState("incom");
+  const [showCalendar, setShowCalendar] = useState(false);
+  const AnimatedValue = useRef(new Animated.Value(0)).current;
+  const animate = (value: number, selected: string) => {
+    setSelected(selected);
+    Animated.timing(AnimatedValue, {
+      duration: 700,
+      toValue: value,
+    }).start();
+  };
+  const headerComponent = () => (
+    <View style={styles.main}>
+      <Text style={styles.header}>{header}</Text>
+      <View style={styles.animatedContainer}>
+        <Animated.View
+          style={{
+            ...styles.animatedView,
+            transform: [{ translateX: AnimatedValue }],
+          }}
+        />
+        <TouchableOpacity
+          disabled={selected === "incom" || showCalendar}
+          onPress={() => animate(0, "incom")}
+          style={styles.selectorContainer}
+        >
+          <Text
+            style={[
+              styles.selectorText,
+              selected === "incom" && styles.selectedStyle,
+            ]}
+          >
+            {animateString1}
+          </Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          disabled={selected === "prev" || showCalendar}
+          onPress={() => {
+            animate(calculatedScreenW / 2, "prev");
+          }}
+          style={styles.selectorContainer}
+        >
+          <Text
+            style={[
+              styles.selectorText,
+              selected === "prev" && styles.selectedStyle,
+            ]}
+          >
+            {animateString2}
+          </Text>
+        </TouchableOpacity>
+      </View>
+      <View style={expStyles.textInputContainer}>
+        <View style={expStyles.searchContainer}>
+          <Image
+            resizeMode="stretch"
+            style={[
+              expStyles.search,
+              {
+                tintColor: DARK_RED,
+              },
+            ]}
+            source={SEARCH}
+          />
+          <TextInput
+            style={expStyles.textInput}
+            editable={!showCalendar}
+            placeholder="Search any product..."
+            placeholderTextColor={"#8D7D75"}
+          />
+        </View>
+        <View style={{ height: "100%" }}>
+          <TouchableOpacity
+            onPress={() => setShowCalendar(!showCalendar)}
+            style={expStyles.exploreBtn}
+          >
+            <Image
+              style={expStyles.explore}
+              resizeMode="contain"
+              source={EXPLORE_BTN}
+            />
+          </TouchableOpacity>
+        </View>
+        {showCalendar && (
+          <TouchableWithoutFeedback>
+            <View style={styles.calendarContainer}>
+              <Calendar />
+            </View>
+          </TouchableWithoutFeedback>
+        )}
+      </View>
+      {additionalHeader}
+    </View>
+  );
+  return (
+    <SafeAreaView style={styles.flex}>
+      <FlatList
+        bounces={false}
+        showsVerticalScrollIndicator={false}
+        data={[1, 2, 3, 5]}
+        ListHeaderComponentStyle={{ zIndex: 1000 }}
+        keyExtractor={(_, i) => String(i)}
+        renderItem={() => children}
+        ListHeaderComponent={headerComponent}
+      />
+    </SafeAreaView>
+  );
+};
+export default CalendarTemplate;
+const styles = StyleSheet.create({
+  calendarContainer: {
+    position: "absolute",
+    top: 40,
+    right: 0,
+    left: 0,
+    zIndex: 100,
+  },
+  text: {
+    fontSize: 17,
+    color: "grey",
+  },
+  animatedView: {
+    position: "absolute",
+    height: "100%",
+    width: calculatedScreenW / 2,
+    backgroundColor: PRIMARY,
+    borderRadius: 30,
+  },
+  selectedStyle: {
+    color: WHITE,
+    fontWeight: "bold",
+  },
+  selectorText: {
+    fontSize: 17,
+    paddingVertical: 20,
+    color: "grey",
+  },
+  flex: {
+    flex: 1,
+  },
+  main: {
+    flex: 1,
+    backgroundColor: LIGHT_GREY,
+    paddingHorizontal: 20,
+    paddingTop: 20,
+  },
+  header: {
+    fontSize: 25,
+    fontWeight: "bold",
+    color: DARK_RED,
+    paddingBottom: 20,
+  },
+  selectorContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  animatedContainer: {
+    flexDirection: "row",
+    width: "100%",
+    backgroundColor: WHITE,
+    borderRadius: 30,
+  },
+});
