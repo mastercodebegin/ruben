31a0a76a366a1fb1f1c439d61ab4ca77b6d1e330
ashishywitmatescom <ashishy@witmates.com>
2023-03-24 15:40:47 +0530

    [1022595] pipeline isse resolved

--- src/packages/framework/src/config.js
+++ src/packages/framework/src/config.js
@@ -1,8 +1,6 @@
 "use strict";
 
-const baseURL = "__MARKER_FOR_BACKEND_URL_REPLACEMENT"
-
-// const baseURL = "https://ruebensftcapp-263982-ruby.b263982.dev.eastus.az.svc.builder.cafe";
+const baseURL = "__MARKER_FOR_BACKEND_URL_REPLACEMENT";
 
 Object.defineProperty(exports, "__esModule", {
   value: true
