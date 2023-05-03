84434a85f61faae0fb4d4f084106605766ec2c7c
selvendiran <selvendiran.r@octavesoftwares.com>
2023-05-03 13:07:09 +0000

    [1064799] validation fix

--- src/packages/blocks/favourites/src/FavouritesController.tsx
+++ src/packages/blocks/favourites/src/FavouritesController.tsx
@@ -4,21 +4,14 @@ import { BlockComponent } from "../../../framework/src/BlockComponent";
 import MessageEnum, {
   getName,
 } from "../../../framework/src/Messages/MessageEnum";
-import AsyncStorage from "@react-native-async-storage/async-storage";
 import { runEngine } from "../../../framework/src/RunEngine";
-
 // Customizable Area Start
 // Customizable Area End
-
 export const configJSON = require("./config");
 
 export interface Props {
   navigation: any;
   id: string;
-  setState: any;
-  state: any;
-  currentUser: string;
-  route: any;
 }
 
 interface S {
@@ -34,8 +27,6 @@ interface S {
   title: string;
   content: string;
   value: any;
-  favouritesList: any;
-  show_loader: boolean;
   // Customizable Area End
 }
 
@@ -77,8 +68,6 @@ export default class FavouritesController extends BlockComponent<Props, S, SS> {
       title: "",
       content: "",
       value: this.value,
-      favouritesList: [],
-      show_loader: false,
     };
     this.onChange = (value) => {
       this.value = value;
@@ -154,21 +143,6 @@ export default class FavouritesController extends BlockComponent<Props, S, SS> {
           this.parseApiErrorResponse(responseJson);
           this.parseApiCatchErrorResponse(errorReponse);
         }
-      } else if (
-        getName(MessageEnum.RestAPIResponceMessage) === message.id &&
-        this.getFavoritesListId != null &&
-        this.getFavoritesListId ===
-        message.getData(getName(MessageEnum.RestAPIResponceDataMessage))
-      ) {
-        const favoritesListData = message.getData(
-          getName(MessageEnum.RestAPIResponceSuccessMessage)
-        );
-        const error = message.getData(
-          getName(MessageEnum.RestAPIResponceErrorMessage)
-        );
-        console.log(" error == == ", favoritesListData);
-        this.setState({ favouritesList: favoritesListData?.data, show_loader: false })
-        console.log(" error == == ", this.state.favouritesList);
       }
     }
     // Customizable Area End
@@ -279,10 +253,6 @@ export default class FavouritesController extends BlockComponent<Props, S, SS> {
     return true;
   };
 
-  getFavoritesListId: string = '';
-  favoritesListProps = {
-    getFavoritesLists: this.getFavoritesList
-  }
   getFavourites = (token: string) => {
     const header = {
       "Content-Type": configJSON.favouritesApiApiContentType,
@@ -310,63 +280,5 @@ export default class FavouritesController extends BlockComponent<Props, S, SS> {
 
     runEngine.sendMessage(requestMessage.id, requestMessage);
   };
-
-  async getFavoritesList() {
-    this.setState({ show_loader: true })
-    const userDetails: any = await AsyncStorage.getItem('userDetails')
-    const tokenData: any = JSON.parse(userDetails)
-    console.log("userdata",tokenData)
-    // const headers = {
-    //   "Content-Type": configJSON.favouritesApiApiContentType,
-    //   'token': tokenData?.meta?.token,
-    // };
-    // var data = {
-    //   "favouriteable_type": "AccountBlock::Account"
-    // };
-    // const getFavoritesListMsg = new Message(
-    //   getName(MessageEnum.RestAPIRequestMessage)
-    // );
-    // this.getFavoritesListId = getFavoritesListMsg.messageId;
-    // getFavoritesListMsg.addData(
-    //   getName(MessageEnum.RestAPIResponceEndPointMessage),
-    //   configJSON.getFavoritesListEndpoint
-    // );
-    // getFavoritesListMsg.addData(
-    //   getName(MessageEnum.RestAPIRequestHeaderMessage),
-    //   JSON.stringify(headers)
-    // );
-    // // getFavoritesListMsg.addData(
-    // //   getName(MessageEnum.RestAPIRequestBodyMessage),
-    // //   data
-    // // );
-    // getFavoritesListMsg.addData(
-    //   getName(MessageEnum.RestAPIRequestMethodMessage),
-    //   configJSON.validationApiMethodType
-    // );
-    // runEngine.sendMessage(getFavoritesListMsg.id, getFavoritesListMsg);
-
-    var myHeaders = new Headers();
-    myHeaders.append("token",tokenData?.meta?.token);
-    
-    var raw = "";
-    
-    var requestOptions = {
-      method: 'GET',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    };
-    
-    fetch("https://ruebensftcapp-263982-ruby.b263982.dev.eastus.az.svc.builder.cafe/bx_block_favourites/favourites", requestOptions)
-      .then(response => response.json())
-      .then(data => {
-        this.setState({ favouritesList: data?.data, show_loader: false })
-        console.log("list== =",this.state.favouritesList);
-      })
-      .catch(error => console.log('error', error));
-  }
-
-
-
   // Customizable Area End
 }
