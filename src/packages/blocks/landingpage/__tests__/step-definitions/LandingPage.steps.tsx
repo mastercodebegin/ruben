f9cb37775ebed25ece8275e00282ffb33e6eedc4
selvendiran <selvendiran.r@octavesoftwares.com>
2023-05-20 04:30:14 +0000

    [1144401] test cases

--- src/packages/blocks/landingpage/__tests__/step-definitions/LandingPage.steps.tsx
+++ src/packages/blocks/landingpage/__tests__/step-definitions/LandingPage.steps.tsx
@@ -1,52 +1,68 @@
-import { defineFeature, loadFeature} from "jest-cucumber"
-import { shallow, ShallowWrapper } from 'enzyme'
+import { defineFeature, loadFeature } from "jest-cucumber";
+import { shallow, ShallowWrapper } from "enzyme";
 
-import * as helpers from '../../../../framework/src/Helpers'
+import * as helpers from "../../../../framework/src/Helpers";
 import React from "react";
-import LandingPage from "../../src/LandingPage"
+import LandingPage from "../../src/LandingPage";
 const navigation = {
-    navigate:jest.fn()
-}
+  navigate: jest.fn(),
+};
 
 const screenProps = {
-    navigation: navigation,
-    id: "LandingPage"
-  }
+  navigation: navigation,
+  id: "LandingPage",
+};
 
-const feature = loadFeature('./__tests__/features/LandingPage-scenario.feature');
+const feature = loadFeature(
+  "./__tests__/features/LandingPage-scenario.feature"
+);
 
 defineFeature(feature, (test) => {
+  beforeEach(() => {
+    jest.resetModules();
+    jest.doMock("react-native", () => ({
+      Platform: { OS: "web" },
+      nativeModule: {},
+    }));
+    jest.spyOn(helpers, "getOS").mockImplementation(() => "web");
+  });
 
+  test("User navigates to LandingPage", ({ given, when, then }) => {
+    let landingPageBlock: ShallowWrapper;
+    let instance: LandingPage;
 
-    beforeEach(() => {
-        jest.resetModules()
-        jest.doMock('react-native', () => ({ Platform: { OS: 'web' },nativeModule:{}}))
-        jest.spyOn(helpers, 'getOS').mockImplementation(() => 'web');
+    given("I am a User loading LandingPage", () => {
+      landingPageBlock = shallow(
+        <LandingPage
+          visible={false}
+          setVisibleProfileModal={function(): void {
+            throw new Error("Function not implemented.");
+          }}
+          setState={undefined}
+          state={undefined}
+          firstTime={false}
+          currentUser={"user"}
+          route={undefined}
+          {...screenProps}
+        />
+      );
     });
 
-    test('User navigates to LandingPage', ({ given, when, then }) => {
-        let landingPageBlock:ShallowWrapper;
-        let instance:LandingPage; 
-
-        given('I am a User loading LandingPage', () => {
-            landingPageBlock = shallow(<LandingPage visible={false} setVisibleProfileModal={function (): void {
-                throw new Error("Function not implemented.")
-            } } setState={undefined} state={undefined} firstTime={false} currentUser={"user"} route={undefined} {...screenProps}/>)
-        });
-
-        when('I navigate to the LandingPage', () => {
-             instance = landingPageBlock.instance() as LandingPage
-        });
-
-        then('LandingPage will load with out errors', () => {
-            expect(landingPageBlock).toBeTruthy();
-        });
-
-        then('I can leave the screen with out errors', () => {
-            instance.componentWillUnmount()
-            expect(landingPageBlock).toBeTruthy();
-        });
+    when("I navigate to the LandingPage", () => {
+      instance = landingPageBlock.instance() as LandingPage;
     });
 
+    then("LandingPage will load with out errors", () => {
+      expect(landingPageBlock).toBeTruthy();
+    });
+    then("I can see the blog posts on landingPage", () => {
+      instance.setState({ imageBlogList: [{}] });
+      instance.forceUpdate();
+    });
 
+    then("I can leave the screen with out errors", () => {
+      instance.componentWillUnmount();
+      expect(landingPageBlock).toBeTruthy();
+    });
+  });
 });
