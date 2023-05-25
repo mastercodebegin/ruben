d6cceb472eef51c5e30aaa16dc5b9822e070bede
selvendiran <selvendiran.r@octavesoftwares.com>
2023-05-25 14:07:42 +0000

    pipeline fix

--- src/packages/framework/src/config.js
+++ src/packages/framework/src/config.js
@@ -1,9 +1,10 @@
-'use strict';
+"use strict";
 
-const baseURL = '__MARKER_FOR_BACKEND_URL_REPLACEMENT';
 
-Object.defineProperty(exports, '__esModule', {
+const baseURL = "__MARKER_FOR_BACKEND_URL_REPLACEMENT";
+
+Object.defineProperty(exports, "__esModule", {
   value: true
 });
 
-exports.baseURL = baseURL;
+exports.baseURL = baseURL; 
