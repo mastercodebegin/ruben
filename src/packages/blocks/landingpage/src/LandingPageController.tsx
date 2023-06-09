26d4539883432c380ca962ab552c434d18c3e0f3
suresh@inevitable-infotech.com <suresh@inevitable-infotech.com>
2023-06-09 06:14:53 +0000

    resolved conflict

--- src/packages/blocks/landingpage/src/LandingPageController.tsx
+++ src/packages/blocks/landingpage/src/LandingPageController.tsx
@@ -925,7 +925,7 @@ export default class LandingPageController extends BlockComponent<
     this.getProductId = getProductListMsg.messageId;
     getProductListMsg.addData(
       getName(MessageEnum.RestAPIResponceEndPointMessage),
-      `${configJSON.addProductEndpoint}${type ? "desc" : "asc"}`
+      `${configJSON.addProductEndpoint}${type ? "asc" : "desc"}`
       );
     getProductListMsg.addData(
       getName(MessageEnum.RestAPIRequestHeaderMessage),
