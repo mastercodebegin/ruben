ba2d2d7357a4d58b93921bd45f2b1c3d9c753056
Suresh Jagnani <suresh@inevitable-infotech.com>
2023-06-20 17:01:56 +0530

    [1277650] payment failed

--- src/packages/blocks/analytics/src/Analytics.tsx
+++ src/packages/blocks/analytics/src/Analytics.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+// Customizable Area Start
 
 import {
   StyleSheet,
@@ -8,12 +9,10 @@ import {
   TouchableWithoutFeedback,
   Button,
   Platform
-  // Customizable Area Start
   , TouchableOpacity,
   Image,
   SafeAreaView,
   Dimensions
-  // Customizable Area End
 } from "react-native";
 
 import AnalyticsController, { Props, configJSON } from "./AnalyticsController";
@@ -22,6 +21,8 @@ import { SCREEN_WIDTH } from "../../../components/src/constants";
 import { colors } from "react-native-elements";
 import { upArrow, downArrow } from "../../categoriessubcategories/src/assets";
 import { store } from "../../../components/src/utils";
+import HeaderWithBackArrowTemplate from "../../../components/src/HeaderWithBackArrowTemplate";
+// Customizable Area End
 
 export default class Analytics extends AnalyticsController {
   constructor(props: Props) {
@@ -35,6 +36,7 @@ export default class Analytics extends AnalyticsController {
 
   render() {
     const isUser = store.getState().currentUser === "user";
+    const { navigation } = this.props;
 
     const data = {
       labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
@@ -73,124 +75,123 @@ export default class Analytics extends AnalyticsController {
     };
     return (
       // Customizable Area Start
-      <ScrollView keyboardShouldPersistTaps="always" 
-      style={styles.container} 
-      bouncesZoom={"No"}>
-        <TouchableWithoutFeedback
-          onPress={() => {
-            this.hideKeyboard();
-          }}
-        >
-          {/* Customizable Area Start */}
-          <SafeAreaView>
-            <View style={styles.main}>
-              <View style={styles.headerContainer}>
-                <TouchableOpacity onPress={() => this.props.navigation.goBack()}>
-                  <Image
-                    style={styles.backImage}
-                    source={require("../../../components/src/arrow_left.png")}
-                  />
-                </TouchableOpacity>
-                <Text style={styles.headerText}>{"Analytics"}</Text>
-              </View>
-              <View style={styles.main}>
-                <View style={styles.chartView}>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                    }} >
-                    <View>
-                      <Text style={styles.totalIncome}>{"Total Income"}</Text>
-                      <Text style={styles.incomeValue}> {"$42,734,00"} </Text>
-                    </View>
-                    <TouchableOpacity
-                        onPress={() => {console.log("Calendar Click")}}
-                      >
-                    <View style={styles.calendarView}>
-                        <Image
-                          style={[styles.backImage, { marginLeft: 10 }]}
-                          source={require("../assets/calendar_icon.png")}
+      <HeaderWithBackArrowTemplate
+        headerText="Analytics"
+        navigation={navigation}
+      >
+        <>
+          <ScrollView keyboardShouldPersistTaps="always"
+            style={styles.container}
+            bouncesZoom={"No"}>
+            <TouchableWithoutFeedback
+              onPress={() => {
+                this.hideKeyboard();
+              }}
+            >
+              {/* Customizable Area Start */}
+              <SafeAreaView>
+                <View style={styles.main}>
+                  <View style={styles.main}>
+                    {isUser}
+                    <View style={styles.chartView}>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                        }} >
+                        <View>
+                          <Text style={styles.totalIncome}>{"Total Income"}</Text>
+                          <Text style={styles.incomeValue}> {"$42,734,00"} </Text>
+                        </View>
+                        <TouchableOpacity
+                          onPress={() => { console.log("Calendar Click") }}
+                        >
+                          <View style={styles.calendarView}>
+                            <Image
+                              style={[styles.backImage, { marginLeft: 10 }]}
+                              source={require("../assets/calendar_icon.png")}
+                            />
+                            <Text style={{ fontSize: 18, color: "#5C2221", marginLeft: 10 }}>
+                              {"March, 2022"}
+                            </Text>
+                          </View>
+                        </TouchableOpacity>
+                      </View>
+                      <View style={{ marginTop: 50 }}>
+                        <BarChart
+                          width={SCREEN_WIDTH}
+                          height={250}
+                          data={data}
+                          fromZero={true}
+                          chartConfig={chartConfig}
+                          showBarTops={false}
+                          withHorizontalLabels={false}
+                          flatColor={true}
+                          withCustomBarColorFromData={true}
+                          verticalLabelRotation={0}
+                          style={{ marginLeft: -60, backgroundColor: "transparent" }}
                         />
-                        <Text style={{ fontSize: 18, color: "#5C2221", marginLeft: 10 }}>
-                          {"March, 2022"}
-                        </Text>
+                        <View style={[styles.overlay, { height: 20 }]} />
+                      </View>
                     </View>
+                    <View style={styles.numberOfSent}>
+                      <Text style={styles.numOfSent}>{"Number of Spend"}</Text>
+                      <View
+                        style={{
+                          flexDirection: "row",
+                          justifyContent: "space-between",
+                        }}
+                      >
+                        <Text style={{ fontSize: 20, color: "#5C2221", fontWeight: "600" }}>
+                          {"243201"}
+                        </Text>
+                        <Text style={{ fontSize: 20, color: "#A0272A" }}>
+                          {"$3344.56"}
+                        </Text>
+                      </View>
+                    </View>
+                    <TouchableOpacity onPress={() => { console.log("click on cow") }}>
+                      <View style={[styles.numberOfSent, { flexDirection: "row", justifyContent: "space-between" }]}>
+                        <Text style={{ color: "#5C2221", fontSize: 17, fontWeight: "600" }}>{"Cow"}</Text>
+                        <Image
+                          style={styles.backImage}
+                          source={downArrow}
+                        />
+                      </View>
                     </TouchableOpacity>
-                  </View>
-                  <View style={{ marginTop: 50 }}>
-                    <BarChart
-                      width={SCREEN_WIDTH}
-                      height={250}
-                      data={data}
-                      fromZero={true}
-                      chartConfig={chartConfig}
-                      showBarTops={false}
-                      withHorizontalLabels={false}
-                      flatColor={true}
-                      withCustomBarColorFromData={true}
-                      verticalLabelRotation={0}
-                      style={{ marginLeft: -60, backgroundColor: "transparent" }}
-                    />
-                    <View style={[styles.overlay, { height: 20 }]} />
-                  </View>
-                </View>
-                <View style={styles.numberOfSent}>
-                  <Text style={styles.numOfSent}>{"Number of Spend"}</Text>
-                  <View
-                    style={{
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                    }}
-                  >
-                    <Text style={{ fontSize: 20, color: "#5C2221", fontWeight: "600" }}>
-                      {"243201"}
-                    </Text>
-                    <Text style={{ fontSize: 20, color: "#A0272A" }}>
-                      {"$3344.56"}
-                    </Text>
-                  </View>
-                </View>
-                <TouchableOpacity onPress={() => {console.log("click on cow")}}>
-                <View style={[styles.numberOfSent, {flexDirection: "row",justifyContent: "space-between"}]}>
-                  <Text style={{ color: "#5C2221", fontSize: 17, fontWeight: "600" }}>{"Cow"}</Text>
-                    <Image
-                      style={styles.backImage}
-                      source={downArrow}
-                    />
-                </View>
-                </TouchableOpacity>
-                <View style={[styles.chartView, {marginTop: 50}]}>
-                </View>
+                    <View style={[styles.chartView, { marginTop: 50 }]}>
+                    </View>
 
-                <View style={styles.boxContainer}>
-                  <View style={styles.box}>
-                    <Text style={styles.boxHeader}>Current Animal purchased</Text>
-                    <Text style={styles.boxText}>Cow</Text>
-                  </View>
-                  <View style={styles.seperator} />
-                  <View style={styles.box}>
-                    <Text style={styles.boxHeader}>Total Cuts</Text>
-                    <Text style={styles.boxText}>10</Text>
+                    <View style={styles.boxContainer}>
+                      <View style={styles.box}>
+                        <Text style={styles.boxHeader}>Current Animal purchased</Text>
+                        <Text style={styles.boxText}>Cow</Text>
+                      </View>
+                      <View style={styles.seperator} />
+                      <View style={styles.box}>
+                        <Text style={styles.boxHeader}>Total Cuts</Text>
+                        <Text style={styles.boxText}>10</Text>
+                      </View>
+                    </View>
+                    <View style={styles.boxContainer}>
+                      <View style={styles.box}>
+                        <Text style={styles.boxHeader}>Used cuts</Text>
+                        <Text style={styles.boxText}>7</Text>
+                      </View>
+                      <View style={styles.seperator} />
+                      <View style={styles.box}>
+                        <Text style={styles.boxHeader}>Remaining Cuts</Text>
+                        <Text style={styles.boxText}>{'3 (10%)'}</Text>
+                      </View>
+                    </View>
                   </View>
                 </View>
-                <View style={styles.boxContainer}>
-                  <View style={styles.box}>
-                    <Text style={styles.boxHeader}>Used cuts</Text>
-                    <Text style={styles.boxText}>7</Text>
-                  </View>
-                  <View style={styles.seperator} />
-                  <View style={styles.box}>
-                    <Text style={styles.boxHeader}>Remaining Cuts</Text>
-                    <Text style={styles.boxText}>{'3 (10%)'}</Text>
-                  </View>
-                </View>
-              </View>
-            </View>
-          </SafeAreaView>
-          {/* Customizable Area End */}
-        </TouchableWithoutFeedback>
-      </ScrollView>
+              </SafeAreaView>
+              {/* Customizable Area End */}
+            </TouchableWithoutFeedback>
+          </ScrollView>
+        </>
+      </HeaderWithBackArrowTemplate>
       // Customizable Area End
     );
   }
