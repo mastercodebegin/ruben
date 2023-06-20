6e61e7ad6cc186c14386df1c655ccd479d7e8d2c
Suresh Jagnani <suresh@inevitable-infotech.com>
2023-06-20 16:04:56 +0530

    [1299193] animal filter dropdown

--- src/packages/blocks/analytics/src/Analytics.tsx
+++ src/packages/blocks/analytics/src/Analytics.tsx
@@ -20,7 +20,8 @@ import AnalyticsController, { Props, configJSON } from "./AnalyticsController";
 import { BarChart } from "react-native-chart-kit";
 import { SCREEN_WIDTH } from "../../../components/src/constants";
 import { colors } from "react-native-elements";
-
+import { upArrow, downArrow } from "../../categoriessubcategories/src/assets";
+import { store } from "../../../components/src/utils";
 
 export default class Analytics extends AnalyticsController {
   constructor(props: Props) {
@@ -33,44 +34,48 @@ export default class Analytics extends AnalyticsController {
   // Customizable Area End
 
   render() {
+    const isUser = store.getState().currentUser === "user";
+
     const data = {
       labels: ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"],
       datasets: [
         {
           data: [60, 45, 28, 80, 99, 43, 80],
-          colors:[
-          (opacity = 1) => `#F8F4F4`,
-          (opacity = 1) => `#F8F4F4`,
-          (opacity = 1) => `#F8F4F4`,
-          (opacity = 1) => `#F8F4F4`,
-          (opacity = 1) => `#5C2221`,
-          (opacity = 1) => `#F8F4F4`,
-          (opacity = 1) => `#F8F4F4`]
+          colors: [
+            (opacity = 1) => `#F8F4F4`,
+            (opacity = 1) => `#F8F4F4`,
+            (opacity = 1) => `#F8F4F4`,
+            (opacity = 1) => `#F8F4F4`,
+            (opacity = 1) => `#5C2221`,
+            (opacity = 1) => `#F8F4F4`,
+            (opacity = 1) => `#F8F4F4`]
         }
       ]
     };
     const chartConfig = {
-       backgroundGradientFrom: 'white',
+      backgroundGradientFrom: 'white',
       // // decimalPlaces: 0,
       // // barPercentage: 1.0,
       // // fillShadowGradientOpacity: 1,
-       backgroundGradientFromOpacity: 0,
-       backgroundGradientTo: 'white',
+      backgroundGradientFromOpacity: 0,
+      backgroundGradientTo: 'white',
       // spacingInner: 1.0,
-      backgroundColor:"transparent",
-       backgroundGradientToOpacity: 0.0,
-      
+      backgroundColor: "transparent",
+      backgroundGradientToOpacity: 0.0,
+
       color: () => `#ffffff`,
       labelColor: () => `black`,
       withShadow: false,
-      barRadius: 15,
+      barRadius: 13,
       propsForBackgroundLines: {
         strokeWidth: 0,
       }
     };
     return (
       // Customizable Area Start
-      <ScrollView keyboardShouldPersistTaps="always" style={styles.container}>
+      <ScrollView keyboardShouldPersistTaps="always" 
+      style={styles.container} 
+      bouncesZoom={"No"}>
         <TouchableWithoutFeedback
           onPress={() => {
             this.hideKeyboard();
@@ -90,23 +95,32 @@ export default class Analytics extends AnalyticsController {
               </View>
               <View style={styles.main}>
                 <View style={styles.chartView}>
-                  <Text style={styles.totalIncome}>{"Total Income"}</Text>
                   <View
                     style={{
                       flexDirection: "row",
                       justifyContent: "space-between",
-                    }}
-                  >
-                    <Text style={{ fontSize: 22, color: "#5C2221", fontWeight: "600" }}>
-                      {"$42,734,00"}
-                    </Text>
-                    <Text style={{ fontSize: 18, color: "#A0272A" }}>
-                      {"$ 334456"}
-                    </Text>
+                    }} >
+                    <View>
+                      <Text style={styles.totalIncome}>{"Total Income"}</Text>
+                      <Text style={styles.incomeValue}> {"$42,734,00"} </Text>
+                    </View>
+                    <TouchableOpacity
+                        onPress={() => {console.log("Calendar Click")}}
+                      >
+                    <View style={styles.calendarView}>
+                        <Image
+                          style={[styles.backImage, { marginLeft: 10 }]}
+                          source={require("../assets/calendar_icon.png")}
+                        />
+                        <Text style={{ fontSize: 18, color: "#5C2221", marginLeft: 10 }}>
+                          {"March, 2022"}
+                        </Text>
+                    </View>
+                    </TouchableOpacity>
                   </View>
-                  <View style={{  marginTop: 50 }}>
+                  <View style={{ marginTop: 50 }}>
                     <BarChart
-                      width={SCREEN_WIDTH }
+                      width={SCREEN_WIDTH}
                       height={250}
                       data={data}
                       fromZero={true}
@@ -116,9 +130,9 @@ export default class Analytics extends AnalyticsController {
                       flatColor={true}
                       withCustomBarColorFromData={true}
                       verticalLabelRotation={0}
-                      style={{marginLeft: -60, backgroundColor: "transparent"}}
+                      style={{ marginLeft: -60, backgroundColor: "transparent" }}
                     />
-                    <View style={[styles.overlay, { height: 20}]} />
+                    <View style={[styles.overlay, { height: 20 }]} />
                   </View>
                 </View>
                 <View style={styles.numberOfSent}>
@@ -129,16 +143,24 @@ export default class Analytics extends AnalyticsController {
                       justifyContent: "space-between",
                     }}
                   >
-                    <Text style={{ fontSize: 18, color: "#5C2221" }}>
+                    <Text style={{ fontSize: 20, color: "#5C2221", fontWeight: "600" }}>
                       {"243201"}
                     </Text>
-                    <Text style={{ fontSize: 18, color: "#A0272A" }}>
-                      {"$ 334456"}
+                    <Text style={{ fontSize: 20, color: "#A0272A" }}>
+                      {"$3344.56"}
                     </Text>
                   </View>
                 </View>
-                <View style={styles.numberOfSent}>
-                  <Text style={{ color: "#5C2221", fontSize: 17 }}>{"Cow"}</Text>
+                <TouchableOpacity onPress={() => {console.log("click on cow")}}>
+                <View style={[styles.numberOfSent, {flexDirection: "row",justifyContent: "space-between"}]}>
+                  <Text style={{ color: "#5C2221", fontSize: 17, fontWeight: "600" }}>{"Cow"}</Text>
+                    <Image
+                      style={styles.backImage}
+                      source={downArrow}
+                    />
+                </View>
+                </TouchableOpacity>
+                <View style={[styles.chartView, {marginTop: 50}]}>
                 </View>
 
                 <View style={styles.boxContainer}>
@@ -230,7 +252,20 @@ const styles = StyleSheet.create({
   },
   backImage: { height: 20, width: 20 },
   totalIncome: {
-    fontSize: 18, color: "#5C2221", paddingBottom: 5, fontWeight: "600",
+    fontSize: 18, color: "#5C2221", paddingBottom: 5, fontWeight: "600", paddingLeft: 16,
+  },
+  incomeValue: {
+    paddingLeft: 16, fontSize: 22, color: "#5C2221", fontWeight: "600"
+  },
+  calendarView: {
+    paddingVertical: 15,
+    borderRadius: 10,
+    height: 50,
+    width: 150,
+    margin: 20,
+    marginTop: 0,
+    backgroundColor: "#F8F4F4",
+    flexDirection: "row",
   },
   flex: { flex: 1 },
   overlay: {
@@ -240,6 +275,6 @@ const styles = StyleSheet.create({
     bottom: 38,
     backgroundColor: 'white',
     width: SCREEN_WIDTH - 40
-  }  
+  }
 });
 // Customizable Area End
