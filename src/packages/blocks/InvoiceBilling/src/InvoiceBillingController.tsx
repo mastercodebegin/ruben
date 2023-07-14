0c6fcbb699a4e5ba292cd223789043e41bf5b4db
selvendiran <selvendiran.r@octavesoftwares.com>
2023-07-14 18:06:05 +0530

    [1350624] code smell fix

--- src/packages/blocks/InvoiceBilling/src/InvoiceBillingController.tsx
+++ src/packages/blocks/InvoiceBilling/src/InvoiceBillingController.tsx
@@ -2,14 +2,14 @@ import { IBlock } from "../../../framework/src/IBlock";
 import { Message } from "../../../framework/src/Message";
 import { BlockComponent } from "../../../framework/src/BlockComponent";
 import MessageEnum, {
-  getName,
+  getName
 } from "../../../framework/src/Messages/MessageEnum";
 import { runEngine } from "../../../framework/src/RunEngine";
 
 // Customizable Area Start
-import { Alert , Linking } from 'react-native';
+import { Alert } from "react-native";
 import { imgPasswordInVisible, imgPasswordVisible } from "./assets";
-import Share from 'react-native-share';
+import Share from "react-native-share";
 import { downloadFiles } from "../../../components/src/utils";
 // Customizable Area End
 
@@ -51,7 +51,7 @@ export default class InvoiceBillingController extends BlockComponent<
 
     // Customizable Area Start
     this.subScribedMessages = [
-      getName(MessageEnum.AccoutLoginSuccess),
+      getName(MessageEnum.AccoutLoginSuccess)
       // Customizable Area Start
       // Customizable Area End
     ];
@@ -61,7 +61,7 @@ export default class InvoiceBillingController extends BlockComponent<
       txtSavedValue: "A",
       enableField: false,
       // Customizable Area Start
-      showLoader: false,
+      showLoader: false
       // Customizable Area End
     };
     runEngine.attachBuildingBlock(this as IBlock, this.subScribedMessages);
@@ -92,13 +92,13 @@ export default class InvoiceBillingController extends BlockComponent<
     onChangeText: (text: string) => {
       this.setState({ txtInputValue: text });
     },
-    secureTextEntry: false,
+    secureTextEntry: false
   };
 
   txtInputMobileProps = {
     ...this.txtInputWebProps,
     autoCompleteType: "email",
-    keyboardType: "email-address",
+    keyboardType: "email-address"
   };
 
   txtInputProps = this.isPlatformWeb()
@@ -112,17 +112,17 @@ export default class InvoiceBillingController extends BlockComponent<
       this.btnShowHideImageProps.source = this.txtInputProps.secureTextEntry
         ? imgPasswordVisible
         : imgPasswordInVisible;
-    },
+    }
   };
 
   btnShowHideImageProps = {
     source: this.txtInputProps.secureTextEntry
       ? imgPasswordVisible
-      : imgPasswordInVisible,
+      : imgPasswordInVisible
   };
 
   btnExampleProps = {
-    onPress: () => this.doButtonPressed(),
+    onPress: () => this.doButtonPressed()
   };
 
   doButtonPressed() {
@@ -144,10 +144,10 @@ export default class InvoiceBillingController extends BlockComponent<
   };
 
   // Customizable Area Start
-  async downloadInvoice(showAlert=false) {
+  async downloadInvoice(showAlert = false) {
     let url;
     try {
-      this.setState({showLoader:true})
+      this.setState({ showLoader: true });
       url = await downloadFiles(
         "https://www.africau.edu/images/default/sample.pdf",
         `${new Date().getTime()}invoice.pdf`,
@@ -156,30 +156,29 @@ export default class InvoiceBillingController extends BlockComponent<
         "invoice",
         true,
         true
-      )
-        this.setState({ showLoader: false })
-        showAlert && Alert.alert('Success', "invoice downloaded in downloads/rubensftcapp")
-    }catch(e:any){
+      );
+      this.setState({ showLoader: false });
+      showAlert &&
+        Alert.alert("Success", "invoice downloaded in downloads/rubensftcapp");
+    } catch (e) {
       Alert.alert("Error", e.message);
-      this.setState({showLoader:false})
-
+      this.setState({ showLoader: false });
     }
     return url;
-   
   }
   async shareInvoice(filePath: string) {
-    console.log('filePathfilePath ',filePath);
-    
+    console.log("filePathfilePath ", filePath);
+
     try {
-      const fileName = 'example.pdf';
+      const fileName = "example.pdf";
       const shareOptions = {
         url: `file://${filePath}`,
         fileName
       };
       await Share.open(shareOptions);
-    } catch (error:any) {
+    } catch (error) {
       // Alert.alert('Error',error.message)
     }
-}
+  }
   // Customizable Area End
 }
