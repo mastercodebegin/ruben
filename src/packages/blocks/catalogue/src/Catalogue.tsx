6a1afc43bf862dfd8fd1f71789d6a6d024752d52
bhaveshm.eai@svam.com <bhaveshm.eai@svam.com>
2023-07-21 14:11:58 +0530

    [1378195] fixed validation

--- src/packages/blocks/catalogue/src/Catalogue.tsx
+++ src/packages/blocks/catalogue/src/Catalogue.tsx
@@ -13,7 +13,6 @@ import Icon from "react-native-vector-icons/FontAwesome";
 // Customizable Area End
 
 import CatalogueController, { Props } from "./CatalogueController";
-
 export default class Catalogue extends CatalogueController {
   constructor(props: Props) {
     super(props);
