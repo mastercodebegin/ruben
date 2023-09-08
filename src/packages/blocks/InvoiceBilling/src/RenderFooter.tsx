7df269fd383cac5dca23d60322b4097281aee961
selvendiran <selvendiran.r@octavesoftwares.com>
2023-09-08 15:11:10 +0530

    coverage increase

--- src/packages/blocks/InvoiceBilling/src/RenderFooter.tsx
+++ src/packages/blocks/InvoiceBilling/src/RenderFooter.tsx
@@ -22,12 +22,12 @@ const RenderFooter = ({ subTotal, total, params }: any) => {
           <Text style={styles.boldText}>Total</Text>
         </View>
         <View>
-          <Text style={[styles.text,{textAlign:"right"}]}>{`$${Number(params?.subTotal)?.toFixed(2)}`}</Text>
+          <Text style={[styles.text,{textAlign:"right"}]}>{`$${Number(params?.subTotal).toFixed(2)}`}</Text>
           <Text style={[styles.text,{textAlign:"right"}]}>{`$${Number(params?.shippingCharge).toFixed(2)}`}</Text>
           {params?.discount ? <Text style={[styles.text,{textAlign:"right"}]}>{`-$${Number(params?.discount).toFixed(2)}`}</Text> : null}
           <Text style={[styles.text, { textAlign: "right" }]}>{`$${Number(params?.deliveryCharge).toFixed(2)}`}</Text>
           {params?.lifetimeSubscription ? <Text style={[styles.text, { textAlign: "right" }]}>{`$${Number(params?.lifetimeSubscription).toFixed(2)}`}</Text> : null}
-          <Text style={[styles.boldText, { textAlign: "right" }]}>{`$${Number(params?.total)?.toFixed(2)}`}</Text>
+          <Text style={[styles.boldText, { textAlign: "right" }]}>{`$${Number(params?.total).toFixed(2)}`}</Text>
         </View>
       </View>
     </View>
