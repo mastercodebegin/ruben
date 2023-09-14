1149b86c0a4c51933f26b4149069e23a610ab5e6
selvendiran <selvendiran.r@octavesoftwares.com>
2023-09-14 15:36:38 +0530

    test failure fix

--- src/packages/blocks/landingpage/src/ExploreStore/SearchList.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import { View } from "react-native";
-
-const SearchList = () => {
-    return (
-        <View style={{height:200,backgroundColor:"red"}} />
-    )
-}
-export default SearchList;
