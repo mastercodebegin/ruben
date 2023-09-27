a3548ca789188e7eb596019cf8700b421d79fc32
naitik.padsala@okoders.com <naitik.padsala@okoders.com>
2023-09-27 09:14:05 +0000

    [1490839]Merchantvisualanalytics

--- src/packages/blocks/analytics/src/Analytics.tsx
+++ src/packages/blocks/analytics/src/Analytics.tsx
@@ -158,7 +158,6 @@ export default class Analytics extends AnalyticsController {
                             </TouchableOpacity>
                           </DisplayCalendar>
 
-
                         </View>
                       </TouchableOpacity>
                     </View>
