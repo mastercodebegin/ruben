390147d2a587672b49234f6a8cf97dd9c2e1e4c6
firoz.k@pixoatic.com <firoz.k@pixoatic.com>
2023-10-26 12:07:12 +0000

    validation resolved

--- src/packages/blocks/landingpage/src/MyCredits/MyCreditDetailsModal.tsx
+++ src/packages/blocks/landingpage/src/MyCredits/MyCreditDetailsModal.tsx
@@ -329,7 +329,7 @@ export default class MyCreditDetailsModal extends LandingPageController {
             visible={this.state.isSuccessPopUp}
             onpressContinue={()=>{this.setState(
               {isSuccessPopUp:false,showMyCreditModal:false})
-            this.props.navigation.dispatch(StackActions.replace('MyOrdersScreen'))}
+            this.props.navigation.dispatch(StackActions.replace('ExplorePage'))}
           }
 
             onpressClose={()=>alert('continue')}
