f41a124de747d88c07eabe708ef7113c03e4cc7d
naitik.padsala@okoders.com <naitik.padsala@okoders.com>
2023-11-10 10:06:28 +0000

    [1772740]testCases

--- src/packages/blocks/landingpage/src/LandingPageController.tsx
+++ src/packages/blocks/landingpage/src/LandingPageController.tsx
@@ -322,7 +322,43 @@ export default class LandingPageController extends BlockComponent<
       let profileDetails = message.getData(
         getName(MessageEnum.RestAPIResponceSuccessMessage)
       );
-      this.profileDetailsCallback(profileDetails);
+      if (profileDetails?.data?.attributes) {
+        const {
+          about_me,
+          email_address,
+          facebook_link,
+          full_name,
+          instagram_link,
+          phone_number,
+          photo,
+          whatsapp_link,
+          id
+        } = profileDetails.data.attributes;
+        this.setState({
+          about_me, email: email_address,
+          facebook_link, name: full_name,
+          instagram_link, phone_number: String(phone_number),
+          profileImage: photo?.url,
+          whatsapp_link,
+          id: id,
+          loader: false
+        })
+        const dispatch = store?.dispatch;
+        dispatch({
+          type: 'PROFILE_DETAILS',
+          payload: {
+            about_me,
+            email_address,
+            facebook_link,
+            full_name,
+            instagram_link,
+            phone_number,
+            photo,
+            whatsapp_link,
+            id
+          }
+        })
+      } 
     } else if (getName(MessageEnum.RestAPIResponceMessage) === message.id &&
       this.getSearchProductId != null &&
       this.getSearchProductId ===
@@ -610,46 +646,6 @@ filterCategoryCallBack(filteredList:any){
     } else {
       this.setState({ productDetails: farmDetails.data[0], show_loader: false })
     }
-  }
-
-  profileDetailsCallback(profileDetails:any){
-    if (profileDetails?.data?.attributes) {
-      const {
-        about_me,
-        email_address,
-        facebook_link,
-        full_name,
-        instagram_link,
-        phone_number,
-        photo,
-        whatsapp_link,
-        id
-      } = profileDetails.data.attributes;
-      this.setState({
-        about_me, email: email_address,
-        facebook_link, name: full_name,
-        instagram_link, phone_number: String(phone_number),
-        profileImage: photo?.url,
-        whatsapp_link,
-        id: id,
-        loader: false
-      })
-      const dispatch = store?.dispatch;
-      dispatch({
-        type: 'PROFILE_DETAILS',
-        payload: {
-          about_me,
-          email_address,
-          facebook_link,
-          full_name,
-          instagram_link,
-          phone_number,
-          photo,
-          whatsapp_link,
-          id
-        }
-      })
-    } 
   }
 
   addToCartCallBack(message:Message) {
