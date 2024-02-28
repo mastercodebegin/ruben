44ef856276353db44adae3f632d3cb59e7be2482
firoz.k@pixoatic.com <firoz.k@pixoatic.com>
2024-02-28 07:44:26 +0000

    notification fix try

--- src/packages/blocks/pushnotifications/src/PushNotificationsHelper.tsx
+++ src/packages/blocks/pushnotifications/src/PushNotificationsHelper.tsx
@@ -74,12 +74,12 @@ export default class PushNotificationsHelper {
     const enabled =
       notificationPermission === messaging.AuthorizationStatus.AUTHORIZED ||
       notificationPermission === messaging.AuthorizationStatus.PROVISIONAL;
-    if (enabled) {
+    // if (enabled) {
       fcmToken = await this.messageInstance.getToken();
       console.log("fcmToken===>",fcmToken);
       // alert(fcmToken)
       setStorageData('fcm_token', fcmToken);
-    }
+    // }
 
     return fcmToken;
   }
